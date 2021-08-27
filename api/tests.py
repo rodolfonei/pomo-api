68a80f33845e7f60e0ad7c89552348fda705038d
RodolfoNei <rodolfonei.webdev@gmail.com>
2021-08-27 19:01:24 -0300

    add create pomo test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Pomo
 
-# Create your tests here.
+class ModelTestCase(TestCase):
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.pomo_name = "Personal Project - Pomo API"
+        self.pomo = Pomo(name=self.pomo_name)
+
+    def test_model_can_create_a_pomo(self):
+        """Test the pomo model can create a pomo."""
+        old_count = Pomo.objects.count()
+        self.pomo.save()
+        new_count = Pomo.objects.count()
+        self.assertNotEqual(old_count, new_count)
