c13ebad89e3cfdddb06afe99b4622a58e57b8bdc
RodolfoNei <rodolfonei.webdev@gmail.com>
2021-09-30 16:11:24 -0300

    implement auth token to requests

--- api/models.py
+++ api/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.dispatch import receiver
 
 class Pomo(models.Model):
     name = models.CharField(max_length=60)
@@ -6,3 +10,8 @@ class Pomo(models.Model):
     start = models.DateTimeField()
     end = models.DateTimeField()
     owner = models.ForeignKey('auth.User', related_name='pomos', on_delete=models.CASCADE) 
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import include, path
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import CreateView, DetailView
 
 urlpatterns = [
     path('auth/', include('rest_framework.urls', namespace='rest_framework')), 
     path('', CreateView.as_view(), name="create"),
-    path('<int:pk>/', DetailView.as_view(), name='detail')
+    path('<int:pk>/', DetailView.as_view(), name='detail'),
+    path('get-token/', obtain_auth_token)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
